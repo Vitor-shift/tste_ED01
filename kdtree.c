3af127137312e78301051311a164fed97cba3be5
Vitor-shift <contatwitch2323@gmail.com>
2025-05-31 14:25:31 -0400

    Update kdtree.c

--- kdtree.c
+++ kdtree.c
@@ -1,39 +1,60 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<float.h>
+#include<float.h> // For DBL_MAX
 #include<string.h>
 #include<assert.h>
+#include<math.h> // For fabs in distance, or for sqrt if actual distance needed
+
+#define EMBEDDING_DIM 128
+#define ID_LEN 100
 
 /*Definições desenvolvedor usuario*/
 typedef struct _reg{
-    int lat;
-    int lon;
-    char nome[100];
+    float embedding[EMBEDDING_DIM];
+    char id[ID_LEN];
 }treg;
 
-void * aloca_reg(int lat, int lon, char nome[]){
-    treg * reg;
-    reg = malloc(sizeof(treg));
-    reg->lat = lat;
-    reg->lon = lon;
-    strcpy(reg->nome,nome);
+void * aloca_reg(const float embedding[EMBEDDING_DIM], const char id[]){
+    treg * reg = malloc(sizeof(treg));
+    if (!reg) {
+        perror("Failed to allocate treg");
+        exit(EXIT_FAILURE);
+    }
+    memcpy(reg->embedding, embedding, EMBEDDING_DIM * sizeof(float));
+    strncpy(reg->id, id, ID_LEN -1);
+    reg->id[ID_LEN -1] = '\0'; 
     return reg;
 }
 
+// Comparador para floats
 int comparador(void *a, void *b, int pos){
-    int ret;
-    if (pos == 0){
-        ret = ((treg *)a)->lat - ((treg *)b)->lat;
-    }else if (pos == 1){
-        ret = ((treg *)a)->lon - ((treg *)b)->lon;
+    treg *reg_a = (treg *)a;
+    treg *reg_b = (treg *)b;
+
+    if (pos < 0 || pos >= EMBEDDING_DIM) {
+       
+        fprintf(stderr, "Error: Invalid dimension access in comparador: %d\n", pos);
+        return 0;
     }
-    return ret;
+
+    if (reg_a->embedding[pos] < reg_b->embedding[pos]){
+        return -1; 
+    } else if (reg_a->embedding[pos] > reg_b->embedding[pos]){
+        return 1; 
+    }
+    return 0;
 }
 
+// Distância Euclidiana Quadrada
 double distancia(void * a, void *b){
-        double ret_lat = ((treg *)a)->lat - ((treg *)b)->lat;
-        double ret_lon = ((treg *)a)->lon - ((treg *)b)->lon;
-        return ret_lat*ret_lat + ret_lon*ret_lon;
+    treg *reg_a = (treg *)a;
+    treg *reg_b = (treg *)b;
+    double sum_sq_diff = 0.0;
+    for (int i = 0; i < EMBEDDING_DIM; ++i){
+        double diff = reg_a->embedding[i] - reg_b->embedding[i];
+        sum_sq_diff += diff * diff;
+    }
+    return sum_sq_diff;
 }
 
 
@@ -52,53 +73,33 @@ typedef struct _arv{
 }tarv;
 
 
-
 /*funções desenvolvedor da biblioteca*/
 
-void kdtree_constroi(tarv * arv, int (*cmp)(void *a, void *b, int ),double (*dist) (void *, void *),int k){
+void kdtree_constroi_arv(tarv * arv, int (*cmp)(void *a, void *b, int ),double (*dist) (void *, void *),int k_dim){
     arv->raiz = NULL;
     arv->cmp = cmp;
     arv->dist = dist;
-    arv->k = k;
+    arv->k = k_dim;
 }
 
-/*teste*/
-void test_constroi(){
-    /* declaracao de variaveis */
-    tarv arv;
-    tnode node1;
-    tnode node2;
-
-    node1.key = aloca_reg(2,3,"Dourados");
-    node2.key = aloca_reg(1,1,"Campo Grande");
-
-
-    /* chamada de funções */
-    kdtree_constroi(&arv,comparador,distancia,2);
-    
-    /* testes */
-    assert(arv.raiz == NULL);
-    assert(arv.k == 2);
-    assert(arv.cmp(node1.key,node2.key,0) == 1);
-    assert(arv.cmp(node1.key,node2.key,1) == 2);
-    assert(strcpy(((treg *)node1.key)->nome,"Dourados"));
-    assert(strcpy(((treg *)node2.key)->nome,"Campo Grande"));
-    free(node1.key);
-    free(node2.key);
-}
-
-void _kdtree_insere(tnode **raiz, void * key, int (*cmp)(void *a, void *b, int),int profund, int k){
+void _kdtree_insere(tnode **raiz, void * key, int (*cmp)(void *a, void *b, int),int profund, int k_dim){
     if(*raiz == NULL){
         *raiz = malloc(sizeof(tnode));
-        (*raiz)->key = key;
+        if (!*raiz) {
+             perror("Failed to allocate tnode for insertion");
+             
+             return;
+        }
+        (*raiz)->key = key; 
         (*raiz)->esq = NULL;
         (*raiz)->dir = NULL;
     }else{
-        int pos = profund % k;
-        if (cmp( (*(*raiz)).key , key ,pos) <0){
-            _kdtree_insere( &((*(*raiz)).dir),key,cmp,profund + 1,k);
+        int pos = profund % k_dim;
+       
+        if (cmp( (*raiz)->key , key ,pos) < 0){ 
+            _kdtree_insere( &((*raiz)->dir),key,cmp,profund + 1, k_dim);
         }else{
-            _kdtree_insere( &((*raiz)->esq),key,cmp,profund +1,k);
+            _kdtree_insere( &((*raiz)->esq),key,cmp,profund + 1, k_dim);
         }
     }
 }
@@ -113,131 +114,132 @@ void _kdtree_destroi(tnode * node){
         _kdtree_destroi(node->esq);
         _kdtree_destroi(node->dir);
         free(node->key);
-        free(node);
+        free(node);      
     }
 }
 
 void kdtree_destroi(tarv *arv){
     _kdtree_destroi(arv->raiz);
+    arv->raiz = NULL;
 }
 
+void _kdtree_busca(tarv *arv, tnode ** atual_node_ptr, void * query_key, int profund, double *menor_dist, tnode **menor_node){
+    tnode * atual_node = *atual_node_ptr;
+    if (atual_node == NULL){
+        return;
+    }
 
-void _kdtree_busca(tarv *arv, tnode ** atual, void * key, int profund, double *menor_dist, tnode **menor){
-    tnode ** lado_principal; 
-    tnode ** lado_oposto;    
-    if (*atual != NULL){
-        double dist_atual = arv->dist((*atual)->key, key);
-        if (dist_atual < *menor_dist){
-            *menor_dist = dist_atual;
-            *menor = *atual;
-        }
-        int pos = profund % arv->k;
-        int comp = arv->cmp(key, (*atual)->key, pos);
+    double dist_atual = arv->dist(atual_node->key, query_key);
+    if (dist_atual < *menor_dist){
+        *menor_dist = dist_atual;
+        *menor_node = atual_node;
+    }
 
-        printf("%s dist %4.3f menor_dist %4.3f comp %d\n", ((treg *)((tnode *)*atual)->key)->nome,dist_atual,*menor_dist,comp);
+    int pos = profund % arv->k;
+    
+    tnode ** lado_principal;
+    tnode ** lado_oposto;
 
-        /* define lado principal para buscar */
-        if (comp < 0){
-            lado_principal =  &((*atual)->esq);
-            lado_oposto    =  &((*atual)->dir); 
-        }else{
-            lado_principal =  &((*atual)->dir);
-            lado_oposto    =  &((*atual)->esq); 
-        }
 
-        _kdtree_busca(arv, lado_principal, key, profund + 1, menor_dist, menor);
+    int comparison_result = arv->cmp(query_key, atual_node->key, pos);
 
-        /* Verifica se deve buscar também no outro lado*/
+    if (comparison_result < 0){
+        lado_principal = &(atual_node->esq);
+        lado_oposto    = &(atual_node->dir);
+    } else {
+        lado_principal = &(atual_node->dir);
+        lado_oposto    = &(atual_node->esq);
+    }
 
-        if (comp*comp < *menor_dist) {
-            printf("tentando do outro lado %f\n",comp*comp);
-            _kdtree_busca(arv, lado_oposto, key, profund + 1, menor_dist, menor);
-        }
+    _kdtree_busca(arv, lado_principal, query_key, profund + 1, menor_dist, menor_node);
+
+    float query_dim_val = ((treg*)query_key)->embedding[pos];
+    float node_dim_val = ((treg*)atual_node->key)->embedding[pos];
+    double diff_dim = query_dim_val - node_dim_val;
+    double dist_sq_to_hyperplane = diff_dim * diff_dim;
+
+
+    if (dist_sq_to_hyperplane < *menor_dist) {
+        _kdtree_busca(arv, lado_oposto, query_key, profund + 1, menor_dist, menor_node);
     }
 }
 
 
-tnode * kdtree_busca(tarv *arv, void * key){
+tnode * kdtree_busca_no(tarv *arv, void * key){
     tnode * menor = NULL;
-    double menor_dist = DBL_MAX;
+    double menor_dist = DBL_MAX; 
+    if (arv->raiz == NULL) return NULL; 
     _kdtree_busca(arv,&(arv->raiz),key,0,&menor_dist,&menor);
     return menor;
 }
 
-treg buscar_mais_proximo(tarv *arv, treg query) {
-    double menor_dist = 1e20;
-    tnode *menor = NULL;
-    _kdtree_busca(arv, &(arv->raiz), &query, 0, &menor_dist, &menor);
-    return *((treg *)(menor->key));
-}
-
 
 tarv arvore_global;
+int arvore_inicializada = 0; 
+
+
+void kdtree_construir() {
+    if (arvore_inicializada) {
+       
+        printf("Arvore KD já inicializada. Reconstruindo...\n");
+         _kdtree_destroi(arvore_global.raiz);
+    }
+    kdtree_constroi_arv(&arvore_global, comparador, distancia, EMBEDDING_DIM);
+    arvore_inicializada = 1;
+    printf("Arvore KD construida com k=%d\n", EMBEDDING_DIM);
+}
 
 tarv* get_tree() {
+    if (!arvore_inicializada) {
+       
+        fprintf(stderr, "Error: Tentativa de obter arvore nao inicializada.\n");
+     
+        kdtree_construir();
+    }
     return &arvore_global;
 }
 
-void inserir_ponto(treg p) {
-    treg *novo = malloc(sizeof(treg));
-    *novo = p;  // cópia de estrutura
-    kdtree_insere(&arvore_global,novo);
-}
-void kdtree_construir() {
-    arvore_global.k = 2;
-    arvore_global.dist = distancia;
-    arvore_global.cmp = comparador;
-    arvore_global.raiz = NULL;
-}
+void inserir_ponto(treg ponto_reg) {
+    if (!arvore_inicializada) {
+        fprintf(stderr, "Atenção: Árvore não inicializada antes da inserção. Construindo agora...\n");
+        kdtree_construir();
+    }
+    // Aloca memória para treg e copia os dados de ponto_reg
+    treg *novo_ponto_heap = malloc(sizeof(treg));
+    if (!novo_ponto_heap) {
+        perror("Falha ao alocar memoria para novo ponto");
+        return;
+    }
+    memcpy(novo_ponto_heap->embedding, ponto_reg.embedding, EMBEDDING_DIM * sizeof(float));
+    strncpy(novo_ponto_heap->id, ponto_reg.id, ID_LEN -1);
+    novo_ponto_heap->id[ID_LEN -1] = '\0';
 
-void test_busca(){
-    tarv arv;
-    kdtree_constroi(&arv,comparador,distancia,2);
-    kdtree_insere(&arv,aloca_reg(10,10,"a"));
-    kdtree_insere(&arv,aloca_reg(20,20,"b"));
-    kdtree_insere(&arv,aloca_reg(1,10,"c"));
-    kdtree_insere(&arv,aloca_reg(3,5,"d"));
-    kdtree_insere(&arv,aloca_reg(7,15,"e"));
-    kdtree_insere(&arv,aloca_reg(4,11,"f"));
-    tnode * raiz = arv.raiz;
-    assert(strcmp(((treg *)raiz->dir->key)->nome, "b")==0);
-    assert(strcmp(((treg *)raiz->esq->key)->nome, "c")==0);
-    assert(strcmp(((treg *)raiz->esq->esq->key)->nome, "d")==0);
-    assert(strcmp(((treg *)raiz->esq->dir->key)->nome, "e")==0);
-
-    printf("\n");
-    treg  * atual = aloca_reg(7,14,"x");
-    tnode * mais_proximo = kdtree_busca(&arv,atual);
-    assert(strcmp(((treg *)mais_proximo->key)->nome,"e") == 0);
-
-    printf("\n");
-    atual->lat = 9;
-    atual->lon = 9;
-    mais_proximo = kdtree_busca(&arv,atual);
-    assert(strcmp(((treg *)mais_proximo->key)->nome,"a") == 0);
-
-    printf("\n");
-    atual->lat = 4;
-    atual->lon = 5;
-    mais_proximo = kdtree_busca(&arv,atual);
-    assert(strcmp(((treg *)mais_proximo->key)->nome,"d") == 0);
-
-    printf("\n");
-    atual->lat = 4;
-    atual->lon = 9;
-    mais_proximo = kdtree_busca(&arv,atual);
-    assert(strcmp(((treg *)mais_proximo->key)->nome,"f") == 0);
-
-
-
-    free(atual);
-    kdtree_destroi(&arv);
+    kdtree_insere(&arvore_global, novo_ponto_heap);
+    printf("Ponto com ID '%s' inserido.\n", novo_ponto_heap->id);
 }
 
 
-int main(void){
-    test_constroi();
-    test_busca();
-    printf("SUCCESS!!\n");
-    return EXIT_SUCCESS;
+treg buscar_mais_proximo(tarv *arv_ptr, treg query_reg) {
+    if (!arv_ptr || !arv_ptr->raiz) {
+        fprintf(stderr, "Erro: Árvore vazia ou não inicializada para busca.\n");
+        treg not_found_reg;
+        memset(&not_found_reg, 0, sizeof(treg));
+        strncpy(not_found_reg.id, "NOT_FOUND", ID_LEN-1);
+        not_found_reg.id[ID_LEN-1] = '\0';
+        return not_found_reg;
+    }
+
+   
+    tnode *mais_proximo_no = kdtree_busca_no(arv_ptr, &query_reg);
+
+    if (mais_proximo_no != NULL && mais_proximo_no->key != NULL) {
+        return *((treg *)(mais_proximo_no->key));
+    } else {
+        fprintf(stderr, "Nenhum ponto encontrado na busca.\n");
+        treg not_found_reg;
+        memset(&not_found_reg, 0, sizeof(treg));
+        strncpy(not_found_reg.id, "NOT_FOUND", ID_LEN-1);
+        not_found_reg.id[ID_LEN-1] = '\0';
+        return not_found_reg;
+    }
 }
